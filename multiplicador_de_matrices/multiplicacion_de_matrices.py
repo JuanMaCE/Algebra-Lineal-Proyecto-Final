5308c9102b32887a9ca1101bcadebd8ab9dfad49
Juan Marcos Cortés Enriquez <124099699+JuanMaCE@users.noreply.github.com>
2024-05-21 11:24:27 -0600

    se quito un archivo que no servia, y se agrego la multiplicación de matrices

--- multiplicador_de_matrices/multiplicacion_de_matrices.py
+++ multiplicador_de_matrices/multiplicacion_de_matrices.py
@@ -1,13 +1,13 @@
 import customtkinter
-global edicion
-global tipo_de_dato
+import numpy as np
+
 global lb_datos
 matriz_a = []
 matriz_b = []
 from CTkMessagebox import CTkMessagebox
 global filas
 global columnas
-
+import numpy
 global lista_a
 global contador_a_columnas
 global contador_a_filas
@@ -29,15 +29,15 @@ def main():
 
     lb_matriz_a = customtkinter.CTkLabel(master=frame, text="", font=("Arial", 50))
     lb_matriz_a.pack(pady=400, padx=400, )
-    lb_matriz_a.place(x=10, y=350)
+    lb_matriz_a.place(x=10, y=275)
 
     lb_matriz_b = customtkinter.CTkLabel(master=frame, text="", font=("Arial", 50))
     lb_matriz_b.pack(pady=400, padx=400, )
-    lb_matriz_b.place(x=250, y=350)
+    lb_matriz_b.place(x=400, y=275)
 
-    lb_matriz_suma_resta = customtkinter.CTkLabel(master=frame, text="", font=("Arial", 50), state="disable")
-    lb_matriz_suma_resta.pack(pady=400, padx=400)
-    lb_matriz_suma_resta.place(x=550, y=350)
+    lb_matriz_multiplicacion = customtkinter.CTkLabel(master=frame, text="", font=("Arial", 50), state="disable")
+    lb_matriz_multiplicacion.pack(pady=400, padx=400)
+    lb_matriz_multiplicacion.place(x=550, y=350)
 
     # ingreso de datos
     ib_ingreso_datos_matriz_a = customtkinter.CTkEntry(master=frame, placeholder_text='Datos Matriz A', width=300,
@@ -66,10 +66,9 @@ def main():
     ib_ingreso_filas_b.place(x=400, y=100)
 
     ib_ingreso_columnas_b = customtkinter.CTkEntry(master=frame, placeholder_text='Datos CDS', width=135,
-                                                 height=35)
+                                                   height=35)
     ib_ingreso_columnas_b.pack(pady=100, padx=10)
     ib_ingreso_columnas_b.place(x=560, y=100)
-
 
     global lista_a
     lista_a = []
@@ -100,7 +99,7 @@ def main():
                                                                    "columnas de b, no son la misma")
 
     def agregar_datos_matriz_a():
-        dato = ib_ingreso_datos_matriz_a.get()
+        dato = float(ib_ingreso_datos_matriz_a.get())
         global lista_a
         lista_a.append(dato)
         global contador_a_columnas
@@ -113,9 +112,14 @@ def main():
             lista_a = []
         if contador_a_filas == int(filas):
             CTkMessagebox(title='ESTA LLENA LA MATRIZ A', message='NO SE PUEDEN AGREGAR MÁS DATOS')
+            matriz_a_impresion = imprimir_matriz(matriz_a)
+            lb_matriz_a.configure(text=" ")
+            lb_matriz_a.configure(state="disabled")
+            lb_matriz_a.configure(text=matriz_a_impresion)
+            lb_matriz_a.configure(state="normal")
 
     def agregar_datos_matriz_b():
-        dato = ib_ingreso_datos_matriz_b.get()
+        dato = float(ib_ingreso_datos_matriz_b.get())
         global lista_b
         lista_b.append(dato)
         global contador_b_columnas
@@ -128,7 +132,11 @@ def main():
             lista_b = []
         if contador_b_filas == int(filas):
             CTkMessagebox(title='ESTA LLENA LA MATRIZ A', message='NO SE PUEDEN AGREGAR MÁS DATOS')
-
+            matriz_a_impresion = imprimir_matriz(matriz_b)
+            lb_matriz_b.configure(text=" ")
+            lb_matriz_b.configure(state="disabled")
+            lb_matriz_b.configure(text=matriz_a_impresion)
+            lb_matriz_b.configure(state="normal")
 
     button_ingreso_datos_matriz_a = customtkinter.CTkButton(master=frame, text='Ingreso Matriz A',
                                                             font=("Arial", 20), fg_color="#3E4446",
@@ -151,6 +159,23 @@ def main():
 
     button_ingreso.pack(pady=10, padx=10)
     button_ingreso.place(x=700, y=100)
+
+    def multiplicar():
+        matriz_resultante = "Resultado : " + "\n"
+        matriz_resultante += str(np.dot(matriz_a, matriz_b))
+        mostrar_multiplicacion = customtkinter.CTkLabel(master=frame, text=matriz_resultante,
+                                                        font=("Times New Roman", 50, "bold"))
+        mostrar_multiplicacion.pack(pady=400, padx=400)
+        mostrar_multiplicacion.place(x=10, y=500)
+        ventana.geometry('950x700')
+
+    button_confirmar = customtkinter.CTkButton(master=frame, text='Multiplicar',
+                                               font=("Arial", 20), fg_color="#3E4446",
+                                               command=multiplicar)
+
+    button_confirmar.pack(pady=10, padx=10)
+    button_confirmar.place(x=700, y=200)
+
 
     # exportar archivos txt
 
@@ -196,3 +221,18 @@ def labels_parte1(frame):
     lb_columnas_matriz_b = customtkinter.CTkLabel(master=frame, text='COLUMNAS MATRIZ B')
     lb_columnas_matriz_b.pack(pady=400, padx=400)
     lb_columnas_matriz_b.place(x=560, y=70)
+
+def imprimir_matriz(matriz):
+    texto = " "
+    for i in matriz:
+        linea = ""
+        contador = 0
+        for x in i:
+            if contador == 0:
+                linea += str(x)
+            else:
+                linea += " | " + str(x)
+            contador += 1
+        texto += linea + "\n"
+    return texto
+
